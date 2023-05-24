5568175c030b3b5b998778de57fb18e728d42646
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-23 22:49:07 -0400

    Change nav for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,10 @@ import iconSmall from '../images/react-icon-small.png';
 export default function Navbar() {
     return (
         <nav>
-            <img src={iconSmall} className="nav--icon" alt="react logo"/>
-            <h3 className="nav--logo_text">ReactFacts</h3>
+            <div class="nav--left">
+                <img src={iconSmall} className="nav--icon" alt="react logo"/>
+                <h3 className="nav--logo_text">ReactFacts</h3>
+            </div>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
