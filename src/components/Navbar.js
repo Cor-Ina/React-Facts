bff9237e3c727b808151e97c76b2aa84de9474cc
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-23 21:05:21 -0400

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import iconSmall from '../images/react-icon-small.png';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={iconSmall} className="nav--icon" alt="react logo"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
