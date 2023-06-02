46b127a42c958f7bbe79ef4c2ed8605e40edf760
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-02 13:31:20 -0400

    Update css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main";
 import './App.css';
 
 export default function App() {
+
     return (
         <div className="container">
             <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
-import iconSmall from '../images/react-icon-small.png';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav>
-            <div class="nav--left">
-                <img src={iconSmall} className="nav--icon" alt="react logo"/>
-                <h3 className="nav--logo_text">ReactFacts</h3>
+        <nav className={props.darkMode ? "dark": ""}>
+            <img className="nav--logo_icon"src="./images/react-icon-small.png" alt=""/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div className="toggler" >
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
             </div>
-            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
